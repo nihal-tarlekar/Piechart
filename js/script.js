fbafaf191300462f7bb9d73f4950ec96579b1fde
tarlekar <51410777+tarlekar@users.noreply.github.com>
2019-07-03 11:00:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,128 @@
+var colors = Highcharts.getOptions().colors,
+    categories = [
+       
+    ],
+    data = [
+        {
+            color: colors[2],
+            drilldown: {
+                name: '',
+                categories: [
+                    '65% remaining'
+                    
+                ],
+                data: [
+                    65
+                    
+                ]
+            }
+        },
+        {
+          
+            color: colors[1],
+            drilldown: {
+                name: '',
+                categories: [
+                    '45% completed'
+                    
+                ],
+                data: [
+                   45
+                ]
+            }
+        },
+    ],
+    browserData = [],
+    versionsData = [],
+    i,
+    j,
+    dataLen = data.length,
+    drillDataLen,
+    brightness;
+
+
+// Build the data arrays
+for (i = 0; i < dataLen; i += 1) {
+
+    // add browser data
+    // browserData.push({
+    //     name: categories[i],
+    //     y: data[i].y,
+    //     color: data[i].color
+    // });
+
+    // add version data
+    drillDataLen = data[i].drilldown.data.length;
+    for (j = 0; j < drillDataLen; j += 1) {
+        brightness = 0.2 - (j / drillDataLen) / 5;
+        versionsData.push({
+            name: data[i].drilldown.categories[j],
+            y: data[i].drilldown.data[j],
+            color: Highcharts.Color(data[i].color).brighten(brightness).get()
+        });
+    }
+}
+
+
+// Create the chart
+Highcharts.chart('container', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market share, January, 2018'
+    },
+    subtitle: {
+        text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+    },
+    plotOptions: {
+        pie: {
+            shadow: false,
+            center: ['50%', '50%']
+        }
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    series: [{
+        name: 'Browsers',
+        data: browserData,
+        size: '60%',
+        dataLabels: {
+            formatter: function () {
+                return this.y > 5 ? this.point.name : null;
+            },
+            color: '#ffffff',
+            distance: -30
+        }
+    }, {
+        name: 'Versions',
+        data: versionsData,
+        size: '80%',
+        innerSize: '60%',
+        dataLabels: {
+            formatter: function () {
+                // display only if larger than 1
+                return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+                    this.y + '%' : null;
+            }
+        },
+        id: 'versions'
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 400
+            },
+            chartOptions: {
+                series: [{
+                }, {
+                    id: 'versions',
+                    dataLabels: {
+                        enabled: false
+                    }
+                }]
+            }
+        }]
+    }
+});
